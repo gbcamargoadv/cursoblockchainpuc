5feafad69fe38fffdda807065796220995622940
Guilherme Bueno <56137587+gbcamargoadv@users.noreply.github.com>
2019-10-10 19:42:16 -0300

    Update Mutuo.sol

--- Mutuo.sol
+++ Mutuo.sol
@@ -1,8 +1,6 @@
 pragma solidity 0.5.12;
-// pode usar essas duas barras para incluir um comentário
-/*
-entre barra asterisco também pode colocar comentários que o compilador descarta
-*/
+
+
 contract Mutuo
 {
 
@@ -11,13 +9,13 @@ contract Mutuo
     uint256 private valor;
     uint256 private reajuste;
     uint256 public parcelas;
-    uint256 constant multaatraso = 2
+    uint256 constant multaatraso = 2;
     
     constructor(
         string memory nomeMutuante,
         string memory nomeMutuario,
-        uint256 valorEmprestimo
-        uint256 jurosMensais
+        uint256 valorEmprestimo,
+        uint256 jurosMensais,
         uint256 numeroParcelas) public
     {
         mutuante = nomeMutuante;
@@ -31,32 +29,33 @@ contract Mutuo
     returns (uint256 valorAtualdaParcela)
     {
         valorAtualdaParcela=valor/parcelas*((1+(reajuste/100)));
-        return valorAtualdaParcela
+        return valorAtualdaParcela;
     }
     
     
     
-    function reajustaParcela(uint256 jurosMensais) public
+    function reajustaParcela(uint256 jurosMensais) public view
     {
         if (jurosMensais > 10)
         {
             jurosMensais = 10;
         }
-        uint256 valorDoAcrescimo = 0;
-        valorDoAcrescimo = ((valor*percentualReajuste)/100);
-        valor = valor+valorDoAcrescimo;
+        
     }
 
     
-    function aplicaMulta (uint256 mesesAtraso) 
+    function aplicaMulta (uint256 mesesAtraso, uint256 BasedeCalculoMulta, uint256 percentualMulta) 
     public view
-    returns(uint256 valorMulta)
+    returns (uint256 valorMulta)
     {
-        require (mesesRestantes=0, "Não há atraso nas parcelas");
+        require (mesesAtraso=0, "Não há atraso nas parcelas");
         for (uint i=1; i<mesesAtraso; i++)
         {
-            valorMulta = valorAtualdaParcela+((valorAtualdaParcela*multaatraso)/100);
-            return valorMulta
+            BasedeCalculoMulta = valorAtualdaParcela;
+            percentualMulta = multaatraso;
+            valorMulta = BasedeCalculoMulta+((BasedeCalculoMulta*percentualMulta)/100);
+            return valorMulta;
         }
 
     }
+}
