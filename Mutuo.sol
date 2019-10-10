20178a79a30fbd74f676d2e126a14280542faa1d
Guilherme Bueno <56137587+gbcamargoadv@users.noreply.github.com>
2019-10-10 20:05:17 -0300

    Update Mutuo.sol

--- Mutuo.sol
+++ Mutuo.sol
@@ -9,20 +9,22 @@ contract Mutuo
     uint256 private valor;
     uint256 private reajuste;
     uint256 public parcelas;
-    uint256 constant multaatraso = 2;
+    uint256 public multaatraso;
     
     constructor(
         string memory nomeMutuante,
         string memory nomeMutuario,
         uint256 valorEmprestimo,
         uint256 jurosMensais,
-        uint256 numeroParcelas) public
+        uint256 numeroParcelas,
+        uint256 percentualMulta) public
     {
         mutuante = nomeMutuante;
         mutuario = nomeMutuario;
         valor = valorEmprestimo;
         reajuste = jurosMensais;
         parcelas = numeroParcelas;
+        multaatraso = percentualMulta;
     }
     
     function valorDaParcela() public view 
@@ -44,16 +46,15 @@ contract Mutuo
     }
 
     
-    function aplicaMulta (uint256 mesesAtraso, uint256 BasedeCalculoMulta, uint256 percentualMulta) 
+    function aplicaMulta (uint256 mesesAtraso, uint256 valorAtualdaParcela, uint256 percentualMulta) 
     public view
     returns (uint256 valorMulta)
     {
-        require (mesesAtraso=0, "Não há atraso nas parcelas");
+        require (mesesAtraso<1, "Não há atraso nas parcelas");
         for (uint i=1; i<mesesAtraso; i++)
         {
-            BasedeCalculoMulta = valorAtualdaParcela;
-            percentualMulta = multaatraso;
-            valorMulta = BasedeCalculoMulta+((BasedeCalculoMulta*percentualMulta)/100);
+            
+            valorMulta = valorAtualdaParcela+((valorAtualdaParcela*percentualMulta)/100);
             return valorMulta;
         }
 
